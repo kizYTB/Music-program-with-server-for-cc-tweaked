19f756b80c2ae5ff694585066c623a0701d73409
kiz___ <140178579+kizYTB@users.noreply.github.com>
2024-12-12 16:21:37 +0100

    Update install.lua

--- install.lua
+++ install.lua
@@ -3,7 +3,7 @@ local run = shell.run
 local function install()
     run("cd","/")
     
-    run("wget","https://raw.githubusercontent.com/kizYTB/Music-program-with-server-for-cc-tweaked/refs/heads/main/config.lua")
+    run("wget","https://raw.githubusercontent.com/kizYTB/Music-program-with-server-for-cc-tweaked/refs/heads/main/config.lua",".config.lua")
 
     run("wget","https://raw.githubusercontent.com/kizYTB/Music-program-with-server-for-cc-tweaked/refs/heads/main/music.lua")
 end
